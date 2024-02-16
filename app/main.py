af5b752ed9c1ed1c9f2ebed506f2896a4d18608b
Dafa <dafaaqilla72@gmail.com>
2024-02-16 21:40:51 +0700

    feat: image blur, debug

--- app/main.py
+++ app/main.py
@@ -1,13 +1,14 @@
 from contextlib import asynccontextmanager
+import time
 import os
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import BackgroundTasks, FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import FileResponse
-from numpy import size
 from pydantic import BaseModel
+from app.body_landmarks.main import FrontBodyLandmarks, SideBodyLandmarks
 
 from app.image_process.main import detect_landmarks, contour, load_image, Mediapipe
-from app.body_landmarks.main import FrontBodyLandmarks, SideBodyLandmarks, FrontAndSideCoords
 from app.body_measurement.main import BodyMeasurement
+from app.schemas.schema import FrontAndSideCoords
 from app.size_recommender.main import SizeRecommendationSystem
 
 detector = {}
@@ -25,40 +26,42 @@ IMAGEDIR = "images/"
 app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/", description="Test")
-async def get_root():
-    return {"message": "Hello World"}
+def cleanup(file_path: str):
+    os.remove(file_path)
 
 
-@app.post("/", description="Test Post")
-async def post_root(text: str,):
-    return {"message": text}
+@app.get("/get_images/", description="Inference landmarks")
+async def download_images(file_path: str, background_tasks: BackgroundTasks):
+    path = IMAGEDIR+file_path
+    try:
+        background_tasks.add_task(cleanup, path)
+        return FileResponse(path, media_type='image/jpeg')
+    except Exception as e:
+        print(e)
+        # Handle file not found or other errors
+        raise HTTPException(status_code=404, detail="File not found")
 
 
 @app.post("/process_images", description="Inference landmarks")
 async def process_images(front_image: UploadFile = File(...), side_image: UploadFile = File(...)):
-
-    FRONT = f"{IMAGEDIR}{front_image.filename}"
-    SIDE = f"{IMAGEDIR}{side_image.filename}"
+    front_path = f"{IMAGEDIR}{time.time()}-{front_image.filename}"
+    side_path = f"{IMAGEDIR}{time.time()}-{side_image.filename}"
 
     # save the file
     front_img = await front_image.read()
-    with open(FRONT, "wb") as f:
+    with open(front_path, "wb") as f:
         f.write(front_img)
 
     side_img = await side_image.read()
-    with open(SIDE, "wb") as f:
+    with open(side_path, "wb") as f:
         f.write(side_img)
 
-    front = load_image(FRONT, 512)
-    side = load_image(SIDE, 512)
+    front = load_image(front_path, 512)
+    side = load_image(side_path, 512)
     front_detection = detect_landmarks(detector["mediapipe"], front)
     side_detection = detect_landmarks(detector["mediapipe"], side)
     front_contours = contour(front_detection)
     side_contours = contour(side_detection)
-
-    os.remove(FRONT)
-    os.remove(SIDE)
 
     if front_detection is None or side_detection is None:
         raise HTTPException(
@@ -69,13 +72,17 @@ async def process_images(front_image: UploadFile = File(...), side_image: Upload
             status_code=500, detail="Error during contour extraction")
 
     front_landmarks = FrontBodyLandmarks(
-        front_detection.pose_landmarks[0], front_contours[-1], front)
+        front_detection.pose_landmarks[0], front_contours[-1], front, debug=True)
     front_landmarks.display_image("front")
+    front_path = front_landmarks.get_image(front_path)
 
     side_landmarks = SideBodyLandmarks(
-        side_detection.pose_landmarks[0], side_contours[-1], side)
+        side_detection.pose_landmarks[0], side_contours[-1], side, debug=True)
     side_landmarks.display_image("side")
-    return FrontAndSideCoords(front=front_landmarks.json(), side=side_landmarks.json())
+    side_path = side_landmarks.get_image(side_path)
+
+    return FrontAndSideCoords(front=front_landmarks.json(), side=side_landmarks.json(), front_path=front_path, side_path=side_path)
+
 
 class MeasureReq(BaseModel):
     actual_height: float
@@ -83,7 +90,7 @@ class MeasureReq(BaseModel):
 
 
 @app.post("/measure_result", description="Get measurement result from the adjusted keypoints")
-async def measure_result(request: MeasureReq):
+async def calculate_measure_result(request: MeasureReq):
     measure_result = BodyMeasurement(
         keypoints=request.adjusted_keypoints, height=request.actual_height)
     measurement_result = measure_result.measure_result()
