7592455398be219a2a01d69419832d09af617db7
Dafa <dafaaqilla72@gmail.com>
2024-02-17 21:34:12 +0700

    feat: gender size & clothing type for size recommedation

--- app/size_recommender/main.py
+++ app/size_recommender/main.py
@@ -1,20 +1,36 @@
-from app.body_measurement.main import MeasureResult
+from typing import Literal
+from app.body_measurement.main import MeasureChart
+from app.size_recommender.size_chart import SizeChart
+from app.size_recommender.weight_chart import WeightChart
+
+ClothingType = Literal["T_SHIRT", "LONG_SHIRT", "SHORT_PANTS", "LONG_PANTS"]
+Gender = Literal["MALE", "FEMALE"]
 
 
 class SizeRecommendationSystem:
-    def __init__(self, customer_measurements: MeasureResult):
-        self.size_chart = {
-            'XS': MeasureResult(height=155, bust_circumference=76, waist_circumference=68, hip_circumference=83.1, shoulder_width=41.6, sleeve_length=54.5, pants_length=93),
-            'S': MeasureResult(height=160, bust_circumference=80, waist_circumference=72, hip_circumference=85.9, shoulder_width=42.8, sleeve_length=55.5, pants_length=96),
-            'M': MeasureResult(height=165, bust_circumference=84, waist_circumference=74, hip_circumference=88.7, shoulder_width=44, sleeve_length=56.5, pants_length=99),
-            'L': MeasureResult(height=170, bust_circumference=88, waist_circumference=78, hip_circumference=91.5, shoulder_width=45.2, sleeve_length=57.5, pants_length=102),
-            'XL': MeasureResult(height=180, bust_circumference=96, waist_circumference=84, hip_circumference=97.1, shoulder_width=47.6, sleeve_length=60, pants_length=108),
-        }
-        self.weights = MeasureResult(height=1, bust_circumference=1, waist_circumference=1,
-                                     hip_circumference=1, shoulder_width=1, sleeve_length=1, pants_length=1)
+    def __init__(self, gender: Gender, clothing_type: ClothingType, customer_measurements: MeasureChart):
+
+        if gender == "MALE":
+            self.size_chart = SizeChart.MAN_SIZE_CHART
+        elif gender == "FEMALE":
+            self.size_chart = SizeChart.WOMAN_SIZE_CHART
+        else:
+            raise ValueError("Invalid gender")
+
+        if clothing_type == 'T_SHIRT':
+            self.weights = WeightChart.T_SHIRT
+        elif clothing_type == 'LONG_SHIRT':
+            self.weights = WeightChart.LONG_SHIRT
+        elif clothing_type == 'SHORT_PANTS':
+            self.weights = WeightChart.SHORT_PANTS
+        elif clothing_type == 'LONG_PANTS':
+            self.weights = WeightChart.LONG_PANTS
+        else:
+            raise ValueError("Invalid clothing type")
+
         self.customer_measurements = customer_measurements
 
-    def calculate_deviation(self, measurements: MeasureResult):
+    def calculate_deviation(self, measurements: MeasureChart):
         deviations: dict = {}
 
         for size, size_measurements in self.size_chart.items():
