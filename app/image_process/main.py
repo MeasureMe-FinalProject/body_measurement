d54fc2756cd90a642d03d5082c47a01e5507bb54
Dafa <dafaaqilla72@gmail.com>
2024-03-06 19:06:46 +0700

    feat: Body Segmented not found handler

--- app/image_process/main.py
+++ app/image_process/main.py
@@ -1,6 +1,9 @@
+from http import HTTPStatus
+
 import cv2
-import numpy as np
 import mediapipe as mp
+import numpy as np
+from fastapi import HTTPException
 
 
 def load_image(file_name, target_width):
@@ -27,16 +30,20 @@ def detect_landmarks(detector, _img):
 
 def contour(detection_result):
     """Define contour using detection result"""
+    if detection_result.segmentation_masks is None:
+        raise HTTPException(
+            status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
+            detail="No body segmentation mask found.",
+        )
     try:
         segmentation_mask = detection_result.segmentation_masks[0].numpy_view()
-        visualized_mask = np.repeat(
-            segmentation_mask[:, :, np.newaxis], 3, axis=2) * 255
+        visualized_mask = (
+            np.repeat(segmentation_mask[:, :, np.newaxis], 3, axis=2) * 255
+        )
 
-        gray = (cv2.cvtColor(visualized_mask, cv2.COLOR_BGR2GRAY))
-        flag, thresh = cv2.threshold(gray.astype(
-            np.uint8), 150, 255, cv2.THRESH_BINARY)
-        contours, _ = cv2.findContours(
-            thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        gray = cv2.cvtColor(visualized_mask, cv2.COLOR_BGR2GRAY)
+        flag, thresh = cv2.threshold(gray.astype(np.uint8), 150, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
         # Draw contours on the image
         contour_image = np.zeros_like(visualized_mask)
@@ -44,5 +51,7 @@ def contour(detection_result):
         cv2.imwrite("res/contour.jpg", contour_image)
         return contours
     except (AttributeError, IndexError) as e:
-        print(f"Error during contour extraction: {e}")
-        return None
+        raise HTTPException(
+            status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+            detail=f"Error during contour extraction: {e}",
+        )
