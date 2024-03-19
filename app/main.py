c704c480101f7ae47d7b318b850efcf1e4aa66f3
Dafa <dafaaqilla72@gmail.com>
2024-03-19 21:12:31 +0700

    feat: Add endpoint for Size Chart

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ from app.image_process.main import contour, detect_landmarks, load_image
 from app.image_process.mediapipe import Mediapipe
 from app.schemas.body_landmarks import FrontAndSideCoordsOut
 from app.schemas.size_recommendation import MeasureResultIn, MeasureResultOut
+from app.size_recommender import size_chart
 from app.size_recommender.main import SizeRecommendationSystem
 
 detector = {}
@@ -21,10 +22,11 @@ detector = {}
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Load the ML model
-    detector['mediapipe'] = Mediapipe().detector
+    detector["mediapipe"] = Mediapipe().detector
     yield
     # Clean up the ML models and release the resources
     detector.clear()
+
 
 IMAGEDIR = "images/"
 app = FastAPI(lifespan=lifespan)
@@ -36,18 +38,24 @@ def cleanup(file_path: str):
 
 @app.get("/get_images/", description="Inference landmarks")
 async def download_images(file_path: str, background_tasks: BackgroundTasks):
-    path = IMAGEDIR+file_path
+    path = IMAGEDIR + file_path
     try:
         background_tasks.add_task(cleanup, path)
-        return FileResponse(path, media_type='image/jpeg')
+        return FileResponse(path, media_type="image/jpeg")
     except Exception as e:
         # Handle file not found or other errors
         raise HTTPException(status_code=404, detail=e) from e
 
 
-@app.post("/process_images", description="Inference landmarks", response_model=FrontAndSideCoordsOut, dependencies=[Depends(ContentTypeChecker(['image/jpeg']))]
-          )
-async def process_images(front_image: UploadFile = File(...), side_image: UploadFile = File(...)):
+@app.post(
+    "/process_images",
+    description="Inference landmarks",
+    response_model=FrontAndSideCoordsOut,
+    dependencies=[Depends(ContentTypeChecker(["image/jpeg"]))],
+)
+async def process_images(
+    front_image: UploadFile = File(...), side_image: UploadFile = File(...)
+):
     front_path = f"{IMAGEDIR}{time.time()}-{front_image.filename}"
     side_path = f"{IMAGEDIR}{time.time()}-{side_image.filename}"
 
@@ -68,37 +76,51 @@ async def process_images(front_image: UploadFile = File(...), side_image: Upload
     side_contours = contour(side_detection)
 
     if front_detection is None or side_detection is None:
-        raise HTTPException(
-            status_code=500, detail="Error during contour extraction")
+        raise HTTPException(status_code=500, detail="Error during contour extraction")
 
     if front_contours is None or side_contours is None:
-        raise HTTPException(
-            status_code=500, detail="Error during contour extraction")
+        raise HTTPException(status_code=500, detail="Error during contour extraction")
 
     front_landmarks = FrontBodyLandmarks(
-        front_detection.pose_landmarks[0], front_contours[-1], front, debug=True)
+        front_detection.pose_landmarks[0], front_contours[-1], front, debug=True
+    )
     front_landmarks.display_image("front")
     front_path = front_landmarks.get_image(front_path)
 
     side_landmarks = SideBodyLandmarks(
-        side_detection.pose_landmarks[0], side_contours[-1], side, debug=True)
+        side_detection.pose_landmarks[0], side_contours[-1], side, debug=True
+    )
     side_landmarks.display_image("side")
     side_path = side_landmarks.get_image(side_path)
 
-    return FrontAndSideCoordsOut(front=front_landmarks.json(), side=side_landmarks.json(), front_path=front_path, side_path=side_path)
+    return FrontAndSideCoordsOut(
+        front=front_landmarks.json(),
+        side=side_landmarks.json(),
+        front_path=front_path,
+        side_path=side_path,
+    )
 
 
-@app.post("/measure_result", description="Get measurement result from the adjusted keypoints",  response_model=MeasureResultOut)
+@app.post(
+    "/measure_result",
+    description="Get measurement result from the adjusted keypoints",
+    response_model=MeasureResultOut,
+)
 async def calculate_measure_result(request: MeasureResultIn):
     measure_result = BodyMeasurement(
-        keypoints=request.adjusted_keypoints,
-        height=request.actual_height)
+        keypoints=request.adjusted_keypoints, height=request.actual_height
+    )
     measurement_result = measure_result.measure_result()
 
     recommended_size = SizeRecommendationSystem(
         gender=request.gender,
         clothing_type=request.clothing_type,
-        customer_measurements=measurement_result
+        customer_measurements=measurement_result,
     ).recommend_size()
 
-    return MeasureResultOut(measurement_result=measurement_result, size_recommendation=recommended_size)
+    return MeasureResultOut(
+        measurement_result=measurement_result, size_recommendation=recommended_size
+    )
+
+
+app.include_router(size_chart.router)
--- app/size_recommender/size_chart.py
+++ app/size_recommender/size_chart.py
@@ -1,20 +1,118 @@
+from fastapi import APIRouter
+
 from app.body_measurement.main import MeasureChart
 
 
 class SizeChart:
     MAN_SIZE_CHART = {
-
-        'XS': MeasureChart(height=155, bust_circumference=76, waist_circumference=68, hip_circumference=83.1, shoulder_width=41.6, sleeve_length=54.5, pants_length=93),
-        'S': MeasureChart(height=160, bust_circumference=80, waist_circumference=72, hip_circumference=85.9, shoulder_width=42.8, sleeve_length=55.5, pants_length=96),
-        'M': MeasureChart(height=165, bust_circumference=84, waist_circumference=74, hip_circumference=88.7, shoulder_width=44, sleeve_length=56.5, pants_length=99),
-        'L': MeasureChart(height=170, bust_circumference=88, waist_circumference=78, hip_circumference=91.5, shoulder_width=45.2, sleeve_length=57.5, pants_length=102),
-        'XL': MeasureChart(height=180, bust_circumference=96, waist_circumference=84, hip_circumference=97.1, shoulder_width=47.6, sleeve_length=60, pants_length=108),
+        "XS": MeasureChart(
+            height=155,
+            bust_circumference=76,
+            waist_circumference=68,
+            hip_circumference=83.1,
+            shoulder_width=41.6,
+            sleeve_length=54.5,
+            pants_length=93,
+        ),
+        "S": MeasureChart(
+            height=160,
+            bust_circumference=80,
+            waist_circumference=72,
+            hip_circumference=85.9,
+            shoulder_width=42.8,
+            sleeve_length=55.5,
+            pants_length=96,
+        ),
+        "M": MeasureChart(
+            height=165,
+            bust_circumference=84,
+            waist_circumference=74,
+            hip_circumference=88.7,
+            shoulder_width=44,
+            sleeve_length=56.5,
+            pants_length=99,
+        ),
+        "L": MeasureChart(
+            height=170,
+            bust_circumference=88,
+            waist_circumference=78,
+            hip_circumference=91.5,
+            shoulder_width=45.2,
+            sleeve_length=57.5,
+            pants_length=102,
+        ),
+        "XL": MeasureChart(
+            height=180,
+            bust_circumference=96,
+            waist_circumference=84,
+            hip_circumference=97.1,
+            shoulder_width=47.6,
+            sleeve_length=60,
+            pants_length=108,
+        ),
     }
 
     WOMAN_SIZE_CHART = {
-        'XS': MeasureChart(height=154, bust_circumference=79, waist_circumference=64, hip_circumference=84, shoulder_width=36, sleeve_length=55, pants_length=88),
-        'S': MeasureChart(height=159, bust_circumference=83, waist_circumference=68, hip_circumference=87, shoulder_width=38, sleeve_length=56, pants_length=90.5),
-        'M': MeasureChart(height=164, bust_circumference=87, waist_circumference=72, hip_circumference=90, shoulder_width=40, sleeve_length=57, pants_length=93.5),
-        'L': MeasureChart(height=169, bust_circumference=91, waist_circumference=76, hip_circumference=93, shoulder_width=42, sleeve_length=58, pants_length=96.5),
-        'XL': MeasureChart(height=174, bust_circumference=95, waist_circumference=81, hip_circumference=97, shoulder_width=44, sleeve_length=59, pants_length=99.5),
+        "XS": MeasureChart(
+            height=154,
+            bust_circumference=79,
+            waist_circumference=64,
+            hip_circumference=84,
+            shoulder_width=36,
+            sleeve_length=55,
+            pants_length=88,
+        ),
+        "S": MeasureChart(
+            height=159,
+            bust_circumference=83,
+            waist_circumference=68,
+            hip_circumference=87,
+            shoulder_width=38,
+            sleeve_length=56,
+            pants_length=90.5,
+        ),
+        "M": MeasureChart(
+            height=164,
+            bust_circumference=87,
+            waist_circumference=72,
+            hip_circumference=90,
+            shoulder_width=40,
+            sleeve_length=57,
+            pants_length=93.5,
+        ),
+        "L": MeasureChart(
+            height=169,
+            bust_circumference=91,
+            waist_circumference=76,
+            hip_circumference=93,
+            shoulder_width=42,
+            sleeve_length=58,
+            pants_length=96.5,
+        ),
+        "XL": MeasureChart(
+            height=174,
+            bust_circumference=95,
+            waist_circumference=81,
+            hip_circumference=97,
+            shoulder_width=44,
+            sleeve_length=59,
+            pants_length=99.5,
+        ),
     }
+
+
+router = APIRouter(
+    prefix="/size_chart",
+    tags=["Size Chart"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.get("/man", response_model=dict[str, MeasureChart])
+async def get_man_size_chart():
+    return SizeChart.MAN_SIZE_CHART
+
+
+@router.get("/woman", response_model=dict[str, MeasureChart])
+async def get_woman_size_chart():
+    return SizeChart.WOMAN_SIZE_CHART
